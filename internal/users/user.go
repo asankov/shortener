8c5dad8c4f0ebb4975f64b936dcf48843cac6860
Anton Sankov <asankov96@gmail.com>
2023-08-12 17:01:10 +0300

    Proper representation of Role in the JWT

--- internal/users/user.go
+++ internal/users/user.go
@@ -1,11 +1,27 @@
 package users
 
+import "encoding/json"
+
+// Role represents a user role.
+//
+// Each role has a set of actions that can perform.
+//
+// The level of permissions is shown by the int value of the role.
+// The lower the value the more permissions a role has.
 type Role int
 
+//go:generate stringer -type=Role -linecomment
 const (
-	RoleAdmin Role = 0
-	RoleUser  Role = 10
+	// RoleAdmin is a role representing an admin.
+	// Its value is 0, which means no role can have greater permissions that it.
+	RoleAdmin Role = 0 // Admin
+	// RoleUser is a role representing a user.
+	RoleUser Role = 10 // User
 )
+
+func (r Role) MarshalJSON() ([]byte, error) {
+	return json.Marshal(r.String())
+}
 
 type User struct {
 	Email string `json:"email"`
