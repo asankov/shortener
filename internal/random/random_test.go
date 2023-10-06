6114d543e1f5b7a89c4cabcc8e4712d67266054d
Anton Sankov <asankov96@gmail.com>
2023-10-06 19:59:50 +0300

    Rename `random/id.go` to `random/random.go`
    because we generate more than IDs in that file.
    
    Add unit tests for that package.

---
+++ internal/random/random_test.go
@@ -0,0 +1,16 @@
+package random_test
+
+import (
+	"testing"
+
+	"github.com/asankov/shortener/internal/random"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRandom(t *testing.T) {
+	id := random.ID(5)
+	require.Len(t, id, 5)
+
+	pwd := random.Password(10)
+	require.Len(t, pwd, 10)
+}
