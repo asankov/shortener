dc453ef22eab7897ea7fa6a5f4cb8c29c5a9f7b0
Anton Sankov <asankov96@gmail.com>
2023-10-07 21:22:58 +0300

    More doc strings
    remove obsolete SSL structs and error

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,27 @@
 package config
 
 import (
-	"errors"
-
 	"github.com/kelseyhightower/envconfig"
 )
 
-var ErrNoSSLConfig = errors.New("no SSL config provided")
-
+// Config is the struct that configures the service.
 type Config struct {
+	// Port controls on which port the service will listen to.
 	Port int `default:"8080"`
-
 	// Secret is the secret used to generate the JWT token.
 	Secret string `required:"true"`
-
+	// UseInMemoryDB controls whether an in-memory DB will be used for the service.
+	//
+	// This is useful for local testing, but not for production use.
 	UseInMemoryDB bool `envconfig:"SHORTENER_USE_IN_MEMORY_DB"`
-
+	// ForceGenerateAdminUser controls whether or not to ALWAYS generate an admin user on startup.
+	//
+	// If true, an admin user will be created on startup.
+	// If false, an admin user might be created due to other conditions.
 	ForceGenerateAdminUser bool `split_words:"true"`
 }
 
-type SSL struct {
-	CertFile string `split_words:"true"`
-	KeyFile  string `split_words:"true"`
-}
-
+// NewFromEnv creates new config with values loaded from environment variables.
 func NewFromEnv() (*Config, error) {
 	var config Config
 	if err := envconfig.Process("SHORTENER", &config); err != nil {
